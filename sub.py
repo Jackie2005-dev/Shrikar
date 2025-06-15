bf7994e9a49cd380fcc7dacdd00d0d7ef8167cf9
Jackie2005-dev <gsshrikar7@gmail.com>
2025-06-15 09:23:27 +0530

    Add files via upload

---
+++ sub.py
@@ -0,0 +1,9 @@
+#Mathematical operations of two numbers
+a = 10
+b = 5
+sum=a+b
+sub=a-b
+div=a/b
+multi=a*b
+print(sum)
+#to print the specific operation change the text in braces after print statement
