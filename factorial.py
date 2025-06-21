639ad80ce74f4f6d27bba0aa3c27d9b95cd30aa0
Jackie2005-dev <gsshrikar7@gmail.com>
2025-06-21 12:56:16 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,7 @@
+import math
+
+def factorial(n):
+    return(math.factorial(n))
+
+num = int(input('Enter a number:'))
+print(factorial(num))
---
+++ mathmodule.py
@@ -0,0 +1,8 @@
+import math
+num= int(input("Enter a number:"))
+square_root=math.sqrt(num)
+result1 = math.log(num)
+result2 = math.sin(num)
+print(square_root)
+print(result1)
+print(result2)
